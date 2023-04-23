910017a8d4f7ca827c2a1ee444a8ebf862a8e754
HP <HP@LAPTOP-NLKKN3QJ.bbrouter>
2023-04-23 22:38:50 +0530

    Hello

---
+++ TestProject/src/encap/Demo.java
@@ -0,0 +1,20 @@
+package encap;
+
+public class Demo {
+
+
+	public static void main(String[] args) {
+		
+		try {
+		System.out.println("Hello");
+		
+			System.out.println(10/0);
+		}
+		
+		finally {
+			System.out.println("Finally block");
+		}
+		
+	}
+
+}
